3684bfad9b195c6d757b5423b624ea3d41798518
Tuan <tuan@Tuans-MacBook-Pro.local>
2024-09-16 17:35:14 +0700

    invitation card.c

---
+++ invitation card.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    printf("\tTHIEP MOI");
+    printf("\nThan moi ban:'Anh tuan bin'");
+    printf("\nToi du le sinh nhat cua minh");
+    printf(" \nVao luc 19h ngay 20/10/2021");
+    printf("\nTai:05/42 M'DRAK-DAKLAK");
+    printf("\nRat mong duoc don tiep!");
+    printf("\nHo Le Thu");
+    return 0;
+}
---
+++ practice.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    printf("BAI THO");
+    printf("Cai trong truong em\n");
+    printf("Mua he\n cung nghi");
+    printf("Suot ba thang lien \t Trong nam nghi");
+    printf("\nBuon khong ha trong?");
+    return 0;
+}
