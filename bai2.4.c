a0886267f271fda38cdbcd836c84e2266b78d48f
Tuan <tuan@Tuans-MacBook-Pro.local>
2024-09-25 10:55:13 +0700

    update

---
+++ bai2.4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,x;
+    printf("Nhap so a: ");
+    scanf("%d",&a);
+    printf("Nhap so b: ");
+    scanf("%d",&b);
+    if(a != 0)
+    {
+        x = -b/a;
+    }
+    if(b != 0)
+    {
+        printf("vo nghiem");
+    }
+    else
+    {
+        printf("vo nghiem");
+    }
+ return 0;   
+}
