51ac408af879b5ba6656ee9b00850590ecc8fa1b
Tuan <tuan@Tuans-MacBook-Pro.local>
2024-09-30 15:45:34 +0700

    cau a

---
+++ bai4.2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    do
+    {   printf("Nhap so n: ");
+        scanf("%d",&n);
+    if(n<=0)
+        printf("\nVui long nhap lai so nguyen duong lon hon\n");
+    }while(n<=0);
+    for(i=1;i<=1;i++)
+    {
+        printf("%d",i);
+
+    }
+    printf("\n");
+   
+
+}
