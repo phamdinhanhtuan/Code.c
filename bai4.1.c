f47ffb2acea4adc315815f840fcd700d72fbf4bc
Tuan <tuan@Tuans-MacBook-Pro.local>
2024-09-30 15:22:25 +0700

    for while do

---
+++ bai4.1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+        printf("\nToi hua khong choi game khi dang hoc\n");
+    i=1;
+    while(i<=10)
+    {
+        printf("\nI DON'T PLAY GAME WHILE STUDYING\n");
+        i++;
+    }
+    do
+    {
+        printf("I ");
+        i+1;
+    }while(i<=10);
+}
